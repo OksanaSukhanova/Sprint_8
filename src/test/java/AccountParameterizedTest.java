bd4f2ccb78957ada081fa5d4663b154e961cea84
Oksana Sukhanova <loksana-92@yandex.ru>
2023-03-23 06:48:44 +0300

    first commit

---
+++ src/test/java/AccountParameterizedTest.java
@@ -0,0 +1,39 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class AccountParameterizedTest {
+    private final String name;
+    private final boolean check;
+
+    public AccountParameterizedTest(String name, boolean check) {
+        this.name = name;
+        this.check = check;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] accountData() {
+        return new Object[][]{
+                {"Yo", false},
+                {"R o", true},
+                {"Ma y", true},
+                {"EighteenSymbols 12", true},
+                {"NineteenSymbol Name", true},
+                {"AlbusSeverus Potter1", false},
+                {" Hermione", false},
+                {"Harry ", false},
+                {"Sirius Black", true}
+        };
+    }
+
+    @Test
+    public void checkNameToEmbossReturnsCorrectValue() {
+        Account accountName = new Account(name);
+        boolean checkName = accountName.checkNameToEmboss();
+        assertEquals("Проверка завершилась некорректно", check, checkName);
+    }
+}
